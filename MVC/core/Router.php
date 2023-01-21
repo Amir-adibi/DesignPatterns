6c8a97e9a564ba28bc499aedeecda6bbfd3aed3b
Amir Adibi <amir.adibinia@gmail.com>
2023-01-21 17:37:46 +0330

    feat: views

--- MVC/core/Router.php
+++ MVC/core/Router.php
@@ -38,9 +38,45 @@ class Router
         $callback = $this->routes[$method][$path] ?? false;
 
         if ($callback === false) {
-            echo "Not found";
+            return "Not found";
         }
 
-        echo call_user_func($callback);
+        if(is_string($callback)) {
+            return $this->renderView($callback);
+        }
+
+        return call_user_func($callback);
+    }
+
+    public function renderView($view)
+    {
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+    }
+
+    protected function layoutContent()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view)
+    {
+        /*
+         * the 'ob_start()' function starts output caching which means
+         * when it is called, nothing is outputted in the browser, instead
+         * the data (which was meant to be outputted in the browser) is cached
+         * in a buffer.
+         *
+         * when the 'ob_get_clean()' function is called, the data stored in
+         * the buffer would be returned and deleted.
+         */
+        
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean();
     }
 }
